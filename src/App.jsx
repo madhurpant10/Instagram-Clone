0e24cd04def2aa74471a08e5c979582b24e19313
madhurpant <madhurp321@gmail.com>
2023-12-03 17:36:55 +0530

    Setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import AuthPage from './pages/AuthPage'
+
+function App() {
+  return (
+    <>
+     <Routes>
+      <Route path='' element={<HomePage />}/>
+      <Route path='/auth' element={<AuthPage />}/>
+     </Routes>
+    </>
+  )
+}
+
+export default App
